dfb2c525e72246d2f0f6f4229c1b6b78b262f226
Felix Cherkasskiy <45081342+fCherkasskiy@users.noreply.github.com>
2019-04-17 13:15:23 -0400

    Update and reformat MapBuilder.py

--- Python/MapBuilder/MapBuilder.py
+++ Python/MapBuilder/MapBuilder.py
@@ -1,15 +1,11 @@
 # Ari Bobesh, Suhas N.
 
-from Tkinter import Tk, Canvas, Button
-from PIL import Image, ImageTk
-from random import *
-import time
-import string
-global filename
+from Tkinter import Tk, Canvas
+
+
 final = 0
 fileopened = 0
 
-filename = "map.txt"
 filename = raw_input("Input filename: ")
 
 if filename == "new":
@@ -27,14 +23,12 @@ elif filename == "final":
 else:
     r = open(filename, "r")
     r2 = r.read().split()
-    size1 = int(r2[0])*10
-    size2 = int(r2[1])*10
+    size1 = int(r2[0]) * 10
+    size2 = int(r2[1]) * 10
     r.close()
     fileopened = 1
 
 w, h = int(size1), int(size2)
-
-global counter, counter2, x, y, x2, y2, tool, color, stop, run, arr
 
 stop = False
 run = True
@@ -47,12 +41,15 @@ y3 = 0
 arr = []
 row = []
 col = []
-colors = ["black", "#ffffff", "red", "blue", "green", "#00ffff", "#ffff00", "#ffa500"]
-
+colors = ["#000000", "#FFFFFF", "#FF0000", "#0000FF", "#00FF00", "#00ffff", "#ffff00", "#ffa500",
+          "#0000A0", "#ADD8E6", "#800080", "#FF00FF", "#808000", "#008000", "#A52A2A", "#808080"]
 counter = 0
 tool = ''
 counter2 = 0
-color="black"
+xin = 0
+yin = 0
+color = "black"
+
 
 root = Tk()
 canvas = Canvas(root, width=w, height=h, bg="white")
@@ -63,11 +60,10 @@ canvas2.pack()
 
 
 def setup(start, listimp):
+    global xin, yin, arr
     root.update()
-    global arr
     o = 0
     arr = []
-    # print listimp
     for wid in range(h / 10):
         row = []
         for leng in range(w / 10):
@@ -76,17 +72,9 @@ def setup(start, listimp):
             else:
                 xin = o % (w / 10)
                 yin = o / (w / 10)
-                # print yin,xin
-                # print o
                 val = int(listimp[o])
                 o += 1
-                # if val!=1:
-                # print "yay"
-
             row.append(val)  # death,powerup,special terrain,color
-            """In the form 1-white,death,powerup 2-white,death,nopowerup..."""
-            """for testing purposes only color: 0-black,1-white,2-red,3-blue,
-               4-green,5-aqua,6-yellow,7-orange"""
         arr.append(row)
     # print arr
 # print arr
@@ -103,41 +91,52 @@ for m in range(w / 10):
 for n in range(h / 10):
     canvas.create_line(0, n * 10, w, n * 10)
 
-canvas2.create_rectangle((2, 400), (40, 440),
-                         outline="black", fill="black")
-
+canvas2.create_rectangle((0, 400), (40, 440),
+                         outline="black", fill="#FFFFFF")  # black
 canvas2.create_rectangle((40, 400), (80, 440),
-                         outline="green", fill="green")
-
-canvas2.create_rectangle((2, 440), (40, 480),
-                         outline="red", fill="red")
-
+                         outline="green", fill="#00FF00")  # green
+canvas2.create_rectangle((0, 440), (40, 480),
+                         outline="red", fill="#FF0000")  # red
 canvas2.create_rectangle((40, 440), (80, 480),
-                         outline="blue", fill="blue")
+                         outline="blue", fill="0000FF")  # blue
 
-canvas2.create_rectangle((2, 400), (40, 320),
-                         outline="#ffffff", fill="#ffffff")
-
-canvas2.create_rectangle((40, 400), (80, 320),
-                         outline="#ffa500", fill="#ffa500")
-
-canvas2.create_rectangle((2, 400), (40, 360),
-                         outline="#ffff00", fill="#ffff00")
-
+canvas2.create_rectangle((0, 360), (40, 320),
+                         outline="#ffffff", fill="#ffffff")  # white
+canvas2.create_rectangle((40, 360), (80, 320),
+                         outline="#ffa500", fill="#ffa500")  # orange
+canvas2.create_rectangle((0, 400), (40, 360),
+                         outline="#ffff00", fill="#ffff00")  # yellow
 canvas2.create_rectangle((40, 400), (80, 360),
-                         outline="#00ffff", fill="#00ffff")
+                         outline="#00ffff", fill="#00ffff")  # cyan
+
+canvas2.create_rectangle((0, 280), (40, 240),
+                         outline="#0000A0", fill="#0000A0")  # d blue
+canvas2.create_rectangle((40, 280), (80, 240),
+                         outline="#ADD8E6", fill="#ADD8E6")  # light blue
+canvas2.create_rectangle((0, 320), (40, 280),
+                         outline="#800080", fill="#800080")  # purple
+canvas2.create_rectangle((40, 320), (80, 280),
+                         outline="#FF00FF", fill="#FF00FF")  # pink
+
+canvas2.create_rectangle((0, 200), (40, 160),
+                         outline="#808000", fill="#808000")  # olive
+canvas2.create_rectangle((40, 200), (80, 160),
+                         outline="#008000", fill="#008000")  # green
+canvas2.create_rectangle((0, 240), (40, 200),
+                         outline="#A52A2A", fill="#A52A2A")  # brown
+canvas2.create_rectangle((40, 240), (80, 200),
+                         outline="#808080", fill="#808080")  # grey
 
 x = 1000
-global pmg6
 
 
 def where_click():
     global tool, color, counter2, stop, run
+    global counter, counter2, x, y, x2, y2, tool, color, stop, run, arr, xin, yin, filename
     counter2 = 0
 
     def getorigin(eventorigin):
-        global counter, x, y, x2, y2, counter2, tool, color, stop, arr
-
+        global counter, counter2, x, y, x2, y2, tool, color, stop, run, arr, xin, yin, filename
         x = eventorigin.x
         y = eventorigin.y
         counter2 = 1
@@ -145,7 +144,6 @@ def where_click():
         root.bind('s', save)
         root.bind('o', openit)
         tool = "paintbrush"
-        # print "tool belt accessed"
         if tool == "paintbrush":
             if x % 10 <= 9 and y % 10 <= 9:
                 x = x - x % 10
@@ -162,10 +160,7 @@ def where_click():
                 arr[x/10][y/10][2]=1
                 arr[x/10][y/10][3]=1
                 print arr[x][y]"""
-        # print "\t\t" + str(stop)
-
     if not stop:
-        # print "click"
         root.bind('<B1-Motion>', getorigin)
         root.bind('<Button-1>', getorigin)
     if run:
@@ -177,7 +172,6 @@ def where_click2():
         global counter, x, y, x2, y2, counter2, tool, color, stop
         x = eventorigin2.x
         y = eventorigin2.y
-        # print "ok"
         if x < 80:
             color = colorpick(x, y, "null")
     root2.bind('<B1-Motion>', getorigin2)
@@ -185,59 +179,43 @@ def where_click2():
     canvas2.after(100, where_click2)
 
 
-def colorpick(x, y, inp):
+def colorpick(_x, _y, inp):
+    global color
     color = "white"
-    # print "yes"
-    if inp == 0:
-        color="black"
-    if inp == 1:
-        color="#ffffff"
-    if inp == 2:
-        color="red"
-    if inp == 3:
-        color="blue"
-    if inp == 4:
-        color="green"
-    if inp == 5:
-        color="#00ffff"
-    if inp == 7:
-        color="#ffa500"
-    if inp == 6:
-        color="#ffff00"
-    if inp == "null":
-        # print "no"
-        if x < 80:
-            if y > 400:
-                if x < 40:
-                    if y < 440:
+    if not inp == "null":
+        color = colors[inp]
+    else:
+        if _x < 80:
+            if _y > 400:
+                if _x < 40:
+                    if _y < 440:
                         color = "black"
                     else:
                         color = "red"
                 else:
-                    if y < 440:
+                    if _y < 440:
                         color = "green"
                     else:
                         color = "blue"
-            elif y < 400:
-                if x < 40:
-                    if y < 360:
+            elif _y < 400:
+                if _x < 40:
+                    if _y < 360:
                         color = "#ffffff"
                     else:
                         color = "#ffff00"
                 else:
-                    if y < 360:
+                    if _y < 360:
                         color = "#ffa500"
                     else:
                         color = "#00ffff"
-    # print color
     return color
 
 
 ##############
-##############
 
 
 def clear(evt):
+    global x, y, x2, y2, x3, y3, counter, tool, counter2
     x = 0
     y = 0
     x2 = 0
@@ -252,11 +230,9 @@ def clear(evt):
 
 
 def save(evt):
-    # print arr
     global filename
     if filename == "new":
         filename = raw_input("Save as: ")
-
     f = open(filename, "w")
     xs = w / 10
     ys = h / 10
@@ -270,30 +246,26 @@ def save(evt):
 
 
 def openit(evt):
+    global xin, yin, color
     f = open(filename, "r")
     inlist = f.read().split()
     xs = int(inlist[0])
     inlist.pop(0)
     ys = int(inlist[0])
     inlist.pop(0)
-    # print "the contents is:"+ str(inlist)
     setup(False, inlist)
     for u in range(xs * ys):
         xin = u % xs
         yin = u / xs
-        # print yin,xin
         tr = int(inlist[u])
-        # print tr
         color = colorpick(100, 0, tr)
         if final == 0:
             canvas.create_rectangle(xin * 10 + 1, yin * 10 + 1, xin * 10 + 9, yin * 10 + 9, outline=color, fill=color)
         else:
             canvas.create_rectangle(xin * 10, yin * 10, xin * 10 + 10, yin * 10 + 10, outline=color, fill=color)
-        # root.update()
     f.close()
 
 
-#############
 where_click()
 where_click2()
 setup(True, False)
