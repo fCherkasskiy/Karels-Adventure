4b337da7aa4d0b91571eb1d4e164c0e2b5a694dc
Felix Cherkasskiy <45081342+fCherkasskiy@users.noreply.github.com>
2019-04-19 10:30:30 -0400

    Update driver.java

--- Java/src/mapLoader/driver.java
+++ Java/src/mapLoader/driver.java
@@ -4,5 +4,6 @@ class driver {
 
     public static void main(String[] args) throws Exception{
         colorReader loader = new colorReader("E:\\TJHSST\\Karels-Adventure\\maps\\dad.txt");
+        
     }
 }
