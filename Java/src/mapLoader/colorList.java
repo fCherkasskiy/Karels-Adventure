35541b4f47b14802a3132475899a33f0d7b68af9
Felix Cherkasskiy <45081342+fCherkasskiy@users.noreply.github.com>
2019-04-18 21:36:05 -0400

    mn bk

---
+++ Java/src/mapLoader/colorList.java
@@ -0,0 +1,16 @@
+package mapLoader;
+
+class colorList {
+    static String[] COLOR_LIST = {
+                "#808000", "#008000",
+                "#A52A2A", "#808080",
+
+                "#0000A0", "#ADD8E6",
+                "#800080", "#FF00FF",
+
+                "white", "#FFA500",
+                "#FFFF00", "#00FFFF",
+
+                "black", "green",
+                "red", "blue"};
+}
--- Java/src/mapLoader/colorReader.java
+++ Java/src/mapLoader/colorReader.java
@@ -3,31 +3,27 @@ package mapLoader;
 import java.io.File;
 import java.util.Scanner;
 
-class colors_mapLoader {
+class colorReader {
 
     private String[] pictureList;
     private int length;
     private int height;
     private int size;
 
-    colors_mapLoader(String path) throws Exception{
+    colorReader(String path) throws Exception{
         File map = new File(path);
         Scanner sc = new Scanner(map);
         length = sc.nextInt();
         height = sc.nextInt();
         size = length * height;
-        String[] pictureList = new String[size];
+        pictureList = new String[size];
         for(int i = 0; i < size; i++) {
-            int a = sc.nextInt();
-            System.out.print(a);
-//            pictureList[i] = driver_mapLoader.COLOR_LIST[a];
-//            System.out.println(pictureList[i]);
-            String g = driver_mapLoader.COLOR_LIST[a];
-            System.out.println(g);
+            pictureList[i] = colorList.COLOR_LIST[sc.nextInt()];
         }
+
     }
 
-    String getColor(int i){return this.pictureList[i];}
+    String getColor(int index){return this.pictureList[index];}
 
     int getLength(){ return length;}
 
